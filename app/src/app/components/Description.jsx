e7af9192b85c876887f06cc5cb963ad992835724
jesusparfer27 <jesusparfer@alumnos.cei.es>
2025-05-13 18:49:46 +0200

    commit

---
+++ app/src/app/components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.module.css'
+
+export default function Description() {
+    return (
+        <div className='flex justify-center my-40'>
+            <p
+            style={{fontFamily: ""}}
+            className='text-[7.5vw] uppercase text-center max-w-[50vw] leading-none pClass'>The quick brown fox jumps over the lazy dog</p>
+        </div>
+    )
+}
